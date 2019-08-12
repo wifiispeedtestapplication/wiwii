fc319df90a2604264ef3dd53627eb29258834a1d
Sukanya <sukanya.c.mohan@gmail.com>
2019-08-12 13:55:27 -0400

    design

---
+++ Wiwii/MasterContentPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Wiwii
+{
+    public partial class MasterContentPage : ContentPage
+    {
+        MasterDetailPage myMaster;
+
+        public MasterContentPage(MasterDetailPage _myMaster)
+        {
+            myMaster = _myMaster;
+            InitializeComponent();
+        }
+    }
+}
---
+++ Wiwii/MasterDetail.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Wiwii
+{
+    public partial class MasterDetail : MasterDetailPage
+    {
+        public MasterDetail()
+        {
+            var mPage = new MasterContentPage(this);
+            Master = mPage;
+            Detail = new NavigationPage(new SettingsPage());
+            InitializeComponent();
+        }
+    }
+}
